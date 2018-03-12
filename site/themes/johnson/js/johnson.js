ab890c2a896761c84a8de85e29752e35e5c67450
Jake Johnson <jake@johnsonjonesgroup.com>
2018-03-12 15:29:04 -0500

    Created callout, button, and plus-icon components

--- site/themes/johnson/js/johnson.js
+++ site/themes/johnson/js/johnson.js
@@ -1,14 +1,28 @@
 $(document).ready(function(){
-    heroResize();
+
+    resize();
     $(window).resize(function(){
-        heroResize();
+        resize();
     });
-    function heroResize(){
+
+    function resize(){
         let heroSize = $(window).height();
-        let resizeAbsolute = $('.absolute-size-of-parent');
-        let resizeParent = resizeAbsolute.parent();
-        resizeParent.css('height', heroSize);
-        resizeAbsolute.css('height', heroSize);
+        let calloutSize = heroSize / 2 + 100;
+        let heroBg = $('.hero-bg');
+        let hero = $('.hero');
+        let calloutBg = $('.callout-bg');
+        let callout = $('.callout');
+        hero.css('height', heroSize);
+        heroBg.css('height', heroSize);
+        callout.css('height', calloutSize);
+        calloutBg.css('height', calloutSize);
     }
+
+    let plusSignSm = '<svg version="1.1" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 40 24" xml:space="preserve" height="24px" width="24px"><polygon fill="#003359" points="19.2,9.6 14.4,9.6 14.4,4.8 14.4,0 9.6,0 9.6,4.8 9.6,9.6 4.8,9.6 0,9.6 0,14.4 4.8,14.4 9.6,14.4 9.6,19.2 9.6,24 14.4,24 14.4,19.2 14.4,14.4 19.2,14.4 24,14.4 24,9.6 "/></svg>';
+    let plusSignMed = '<svg version="1.1" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 38 24" xml:space="preserve" height="48px" width="48px"><polygon fill="#003359" points="19.2,9.6 14.4,9.6 14.4,4.8 14.4,0 9.6,0 9.6,4.8 9.6,9.6 4.8,9.6 0,9.6 0,14.4 4.8,14.4 9.6,14.4 9.6,19.2 9.6,24 14.4,24 14.4,19.2 14.4,14.4 19.2,14.4 24,14.4 24,9.6 "/></svg>';
+    let plusSignLrg = '<svg version="1.1" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="4 0 18 24" xml:space="preserve" height="64px" width="64px"><polygon fill="#003359" points="19.2,9.6 14.4,9.6 14.4,4.8 14.4,0 9.6,0 9.6,4.8 9.6,9.6 4.8,9.6 0,9.6 0,14.4 4.8,14.4 9.6,14.4 9.6,19.2 9.6,24 14.4,24 14.4,19.2 14.4,14.4 19.2,14.4 24,14.4 24,9.6 "/></svg>';  
+    $('.plus, .plus-sm').replaceWith(plusSignSm);
+    $('.plus-md').replaceWith(plusSignMed);
+    $('.plus-lg').replaceWith(plusSignLrg);
 
 });
