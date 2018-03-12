030983df9c12c8d3e11ac5979e0c4d09c09471fd
Jake Johnson <jake@johnsonjonesgroup.com>
2018-03-12 18:02:49 -0500

    Added responsiveness to components

--- site/themes/johnson/js/johnson.js
+++ site/themes/johnson/js/johnson.js
@@ -1,21 +1,25 @@
 $(document).ready(function(){
 
-    resize();
+    let heroSize = $(window).height() -  $('.nav').height();
+    let hero =$('.hero');
+    let heroBg = $('.hero-bg');
+    resize(heroSize, hero, heroBg);
+
+    let calloutSize = heroSize / 2;
+    let callout =$('.callout');
+    let calloutBg = $('.callout-bg');
+    resize(calloutSize, callout, calloutBg);
+
     $(window).resize(function(){
-        resize();
+        resize(heroSize, hero, heroBg);
+        resize(calloutSize, callout, calloutBg);
     });
 
-    function resize(){
-        let heroSize = $(window).height();
-        let calloutSize = heroSize / 2 + 100;
-        let heroBg = $('.hero-bg');
-        let hero = $('.hero');
-        let calloutBg = $('.callout-bg');
-        let callout = $('.callout');
-        hero.css('height', heroSize);
-        heroBg.css('height', heroSize);
-        callout.css('height', calloutSize);
-        calloutBg.css('height', calloutSize);
+
+
+    function resize(target, parent, bg){
+            parent.css('min-height', target);
+            bg.css('min-height', parent.innerHeight());
     }
 
     let plusSignSm = '<svg version="1.1" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 40 24" xml:space="preserve" height="24px" width="24px"><polygon fill="#003359" points="19.2,9.6 14.4,9.6 14.4,4.8 14.4,0 9.6,0 9.6,4.8 9.6,9.6 4.8,9.6 0,9.6 0,14.4 4.8,14.4 9.6,14.4 9.6,19.2 9.6,24 14.4,24 14.4,19.2 14.4,14.4 19.2,14.4 24,14.4 24,9.6 "/></svg>';
