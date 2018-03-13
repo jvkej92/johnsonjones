5c778ec8e635a8df4be8dcc9d3295c84255803e8
Jake Johnson <jake@johnsonjonesgroup.com>
2018-03-13 09:44:26 -0500

    Minor tweaks to navigation

--- site/themes/johnson/js/johnson.js
+++ site/themes/johnson/js/johnson.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     $('.mobile-menu-items').css('height', $(window).height - $('.nav-bar').height());
-    let heroSize = $(window).height() - $('.nav').height();
+    let heroSize = $(window).height();
     let hero = $('.hero');
     let heroBg = $('.hero-bg');
     resize(heroSize, hero, heroBg);
@@ -24,6 +24,8 @@ $(document).ready(function () {
     let plusSignSm = '<svg version="1.1" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 40 24" xml:space="preserve" height="24px" width="24px"><polygon fill="#003359" points="19.2,9.6 14.4,9.6 14.4,4.8 14.4,0 9.6,0 9.6,4.8 9.6,9.6 4.8,9.6 0,9.6 0,14.4 4.8,14.4 9.6,14.4 9.6,19.2 9.6,24 14.4,24 14.4,19.2 14.4,14.4 19.2,14.4 24,14.4 24,9.6 "/></svg>';
     let plusSignMed = '<svg version="1.1" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 38 24" xml:space="preserve" height="48px" width="48px"><polygon fill="#003359" points="19.2,9.6 14.4,9.6 14.4,4.8 14.4,0 9.6,0 9.6,4.8 9.6,9.6 4.8,9.6 0,9.6 0,14.4 4.8,14.4 9.6,14.4 9.6,19.2 9.6,24 14.4,24 14.4,19.2 14.4,14.4 19.2,14.4 24,14.4 24,9.6 "/></svg>';
     let plusSignLrg = '<svg version="1.1" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="4 0 18 24" xml:space="preserve" height="64px" width="64px"><polygon fill="#003359" points="19.2,9.6 14.4,9.6 14.4,4.8 14.4,0 9.6,0 9.6,4.8 9.6,9.6 4.8,9.6 0,9.6 0,14.4 4.8,14.4 9.6,14.4 9.6,19.2 9.6,24 14.4,24 14.4,19.2 14.4,14.4 19.2,14.4 24,14.4 24,9.6 "/></svg>';
+    
+    let logo = 
     $('.plus, .plus-sm').replaceWith(plusSignSm);
     $('.plus-md').replaceWith(plusSignMed);
     $('.plus-lg').replaceWith(plusSignLrg);
@@ -37,6 +39,18 @@ $(document).ready(function () {
                 $('.mobile-menu-items').slideDown(100);
             }
         });
+
+    $(window).scroll(function(){
+        console.log($(window).scrollTop())
+        if($(window).scrollTop() >heroSize - 70){
+            $('.nav').addClass('nav-scroll');
+            $('.nav-logo img').attr('src', 'assets/img/johnsonJonesLogoNavy.png')
+        }
+        else{
+            $('.nav').removeClass('nav-scroll');
+            $('.nav-logo img').attr('src', 'assets/img/johnsonJonesLogoWhite.png')
+        }
+    });
 
 
     });
